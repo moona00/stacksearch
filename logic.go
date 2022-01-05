4ca79c7bbed060b357fe31f44c4de0648f00b6ca
shadowboi19 <56690537+shadowboi19@users.noreply.github.com>
2022-01-05 09:20:40 +0100

    finished

--- logic.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// SortOptions the sort options
-var SortOptions = map[string]string{
-	"-r": "relevance",
-	"-n": "newest",
-	"-a": "active",
-	"-v": "votes",
-}
--- main.go
+++ main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/gocolly/colly"
 )
+
+const (
+	inputError   = "Input error"
+	browserError = "Browser error"
+)
+
+var sortOptions = map[string]string{
+	"-r": "relevance",
+	"-n": "newest",
+	"-a": "active",
+	"-v": "votes",
+}
 
 func main() {
 	var msg, sort string
@@ -14,9 +27,9 @@ func main() {
 	case 1:
 		msg = "Please insert something to search"
 	case 2:
-		sort = SortOptions["-r"]
+		sort = sortOptions["-r"]
 	case 3:
-		s, ok := SortOptions[os.Args[2]]
+		s, ok := sortOptions[os.Args[2]]
 		sort = s
 		if !ok {
 			msg = "Invalid sort option"
@@ -30,6 +43,42 @@ func main() {
 		os.Exit(1)
 	}
 
-	c := colly.NewCollector()
-	c.Visit(fmt.Sprintf("https://www.stackoverflow.com/search?tab=%s&q=%s", sort, os.Args[1]))
+	col := colly.NewCollector()
+
+	i := 0
+	links := make([]string, 15)
+
+	col.OnHTML("#questions .question-hyperlink", func(e *colly.HTMLElement) {
+		fmt.Printf("%d %s\n", i, e.Text)
+		links[i] = e.Attr("href")
+		i++
+	})
+
+	url := fmt.Sprintf("https://www.stackoverflow.com/questions/tagged/%s?tab=%s", os.Args[1], sort)
+	fmt.Println(url)
+
+	err := col.Visit(url)
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	var inputLink int
+	_, err = fmt.Scan(&inputLink)
+	if err != nil {
+		panic(inputError)
+	}
+
+	err = os.Chdir("/")
+	if err != nil {
+		panic(browserError)
+	}
+
+	cmd := []string{
+		"open",
+		fmt.Sprintf("https://www.stackoverflow.com%s", links[inputLink]),
+	}
+	err = exec.Command(cmd[0], cmd[1]).Start()
+	if err != nil {
+		panic(err)
+	}
 }
